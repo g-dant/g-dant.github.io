28c43af3212a3bc02babf5f60ab4713c86ac70de
g-dant <guilhermedantas@poli.ufrj.br>
2022-09-04 13:54:51 -0300

    Enhancing projects table - multiple languages support

--- assets/js/list_repos.js
+++ assets/js/list_repos.js
@@ -1,14 +1,17 @@
-var newRow = function(rowText) {
+var newRow = function(rowText1,rowText2) {
     var tr = document.createElement('tr');
-    var td = document.createElement('td');
-    td.innerText = rowText;
-    tr.appendChild(td);
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    td1.innerText = rowText1;
+    td2.innerText = rowText2;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
     return tr;
 }
 
 var repos = fetch('https://api.github.com/users/g-dant/repos').
     then(resp => resp.json()).then(function(dataArray){
         dataArray.forEach((data) => { 
-            document.getElementById('projects-node').appendChild(newRow(data.name));
+            document.getElementById('projects-node').appendChild(newRow(data.name, data.description));
         });
     });
