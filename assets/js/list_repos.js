ba5279438769617275ec454022e5025a8eee81b9
g-dant <guilhermedantas@poli.ufrj.br>
2022-09-04 14:01:00 -0300

    Adding hyperlink to projects names

--- assets/js/list_repos.js
+++ assets/js/list_repos.js
@@ -1,8 +1,8 @@
-var newRow = function(rowText1,rowText2) {
+var newRow = function(rowText1,rowText2, rowText1URL) {
     var tr = document.createElement('tr');
     var td1 = document.createElement('td');
     var td2 = document.createElement('td');
-    td1.innerText = rowText1;
+    td1.innerText = '<a href="' + rowText1URL + '">' + rowText1 + '</a>';
     td2.innerText = rowText2;
     tr.appendChild(td1);
     tr.appendChild(td2);
@@ -12,6 +12,6 @@ var newRow = function(rowText1,rowText2) {
 var repos = fetch('https://api.github.com/users/g-dant/repos').
     then(resp => resp.json()).then(function(dataArray){
         dataArray.forEach((data) => { 
-            document.getElementById('projects-node').appendChild(newRow(data.name, data.description));
+            document.getElementById('projects-node').appendChild(newRow(data.name, data.description, data.html_url));
         });
     });
