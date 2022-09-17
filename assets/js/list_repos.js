690a83c3481300e0eccbdfecb961c0786300ef05
g-dant <guilhermedantas@poli.ufrj.br>
2022-09-17 13:09:27 -0300

    If a project as website, the href of the table rows will point to it

--- assets/js/list_repos.js
+++ assets/js/list_repos.js
@@ -1,20 +1,33 @@
-var newRow = function(rowText1,rowText2, rowText1URL) {
-    var tr = document.createElement('tr');
-    var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-    var a = document.createElement('a');
-    a.href = rowText1URL;
-    a.innerText = rowText1;
-    td1.appendChild(a);
-    td2.innerText = rowText2;
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    return tr;
+var github_user_name = 'g-dant'
+var github_api_link = 'https://api.github.com/users/' + github_user_name + '/repos';
+var githubProjectLink = project_name => 'https://' + github_user_name +'.github.io/' + project_name;
+
+var thereIsProjectLink = async function(project_name) {
+    return fetch(githubProjectLink(project_name)).then(resp => resp.status);
 }
 
-var repos = fetch('https://api.github.com/users/g-dant/repos').
-    then(resp => resp.json()).then(function(dataArray){
-        dataArray.forEach((data) => { 
-            document.getElementById('projects-node').appendChild(newRow(data.name, data.description, data.html_url));
-        });
+var newRow = async function(rowText1,rowText2, rowText1URL, project_name) {
+    return thereIsProjectLink(project_name).then(() => {
+        var tr = document.createElement('tr');
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var a = document.createElement('a');
+        a.href = restp.status == '200' ? rowText1URL : githubProjectLink(project_name);
+        a.innerText = rowText1;
+        td1.appendChild(a);
+        td2.innerText = rowText2;
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        return tr;
     });
+}
+
+var repos = fetch(github_api_link).
+    then(resp => resp.json()).then(function(dataArray){
+        var promise_array = [];
+        dataArray.forEach((data) => { 
+            promise_array.push(newRow(data.name, data.description, data.html_url));
+        });
+        return promise_array.all();
+    }).then(elementsArray => 
+        elementsArray.forEach(element => document.getElementById('projects-node').appendChild(element)));
